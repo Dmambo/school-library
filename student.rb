0e162edbe341e36197201d98d06852a9e9aa892a
Dmambo <mambotrader0@gmail.com>
2023-03-13 20:41:42 +0530

    Fixed linter errors

--- student.rb
+++ student.rb
@@ -3,12 +3,12 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, parent_permission = true, name = "Unknown", classroom)
+  def initialize(age, parent_permission = true, name = 'Unknown', classroom)
     super(age, parent_permission, name)
     @classroom = classroom
   end
 
   def play_hooky
-    "¯\\_(ツ)_/¯"
+    '¯\\_(ツ)_/¯'
   end
 end
