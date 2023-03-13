075d40696613ebf1dbafd6c1a66fca4c362d9e80
Dmambo <mambotrader0@gmail.com>
2023-03-13 22:24:09 +0530

    Created Library foundation

---
+++ person.rb
@@ -0,0 +1,23 @@
+class Person
+    def initialize(age, name: 'Unknown', parent_permission: true)
+      @age = age
+      @name = name
+      @parent_permission = parent_permission
+    end
+  
+    def can_use_services?
+      @parent_permission || is_of_age?
+    end
+  
+    def validate_name
+      @name = 'Unknown' if @name.nil?
+    end
+  
+    def validate_age
+      @age = 0 if @age.negative?
+    end
+  
+    def of_age?
+      @age >= 18
+    end
+  end
---
+++ student.rb
@@ -0,0 +1,14 @@
+require_relative 'person'
+
+class Student < Person
+  attr_reader :classroom
+
+  def initialize(age, classroom:, **rest)
+    super(age, **rest)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\\_(ツ)_/¯'
+  end
+end
