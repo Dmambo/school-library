7be97b07321a6934c97926dc4e3069d93b7244af
tsohleDev <mokhemisitsohle@gmail.com>
2023-03-21 10:54:54 +0200

    Move run method to Main

--- app.rb
+++ app.rb
@@ -12,30 +12,6 @@ class App
     @rentals = []
   end
 
-  # rubocop:disable Metrics/CyclomaticComplexity
-  def run(display_menu)
-    loop do
-      print 'hello'
-      display_menu
-      choice = gets.chomp.to_i
-      case choice
-      when 1 then list_books
-      when 2 then list_people
-      when 3 then create_person
-      when 4 then create_book
-      when 5 then create_rental
-      when 6 then list_rentals_for_person
-      when 7
-        puts 'Thanks for using this app'
-        exit
-      else puts 'Invalid option, try again.'
-      end
-    end
-  end
-  # rubocop:enable Metrics/CyclomaticComplexity
-
-  private
-
   def list_books
     puts 'List of all books:'
     @books.each do |book|
@@ -106,35 +82,6 @@ class App
     puts
   end
 
-  def create_rental
-    puts 'Select a book from the following list (enter the corresponding number):'
-    @books.each.with_index(1) do |book, i|
-      puts "#{i}. #{book.title} by #{book.author}"
-    end
-    book_choice = gets.chomp.to_i
-    book = @books[book_choice - 1]
-    if book.nil?
-      puts 'Invalid book selection.'
-      return
-    end
-    puts 'Select a person from the following list (enter the corresponding number):'
-    @people.each.with_index(1) do |person, i|
-      puts "#{i}. [#{person.class}] name: #{person.name}, ID: #{person.id}, age: #{person.age}"
-    end
-    person_choice = gets.chomp.to_i
-    person = @people[person_choice - 1]
-    if person.nil?
-      puts 'Invalid person selection.'
-      return
-    end
-    puts 'Enter the date the rental starts (yyyy-mm-dd):'
-    start_date = Date.parse(gets.chomp)
-    rental = Rental.new(book, person, start_date)
-    @rentals << rental
-    puts "Rental created successfully\n"
-    puts
-  end
-
   def list_rentals_for_person
     puts 'Enter the ID of the person:'
     person_id = gets.chomp.to_i
@@ -161,5 +108,46 @@ class App
   def find_rental_by_id(id)
     @rentals.find { |rental| rental.id == id }
   end
-end
 
+  def create_rental
+    book = book_by_choice
+    if book.nil?
+      puts 'Invalid book selection.'
+      return
+    end
+
+    person = person_by_choice
+    if person.nil?
+      puts 'Invalid person selection.'
+      return
+    end
+    puts 'Enter the date the rental starts (yyyy-mm-dd):'
+    start_date = Date.parse(gets.chomp)
+    rental = Rental.new(book, person, start_date)
+    @rentals << rental
+    puts "Rental created successfully\n"
+    puts
+  end
+
+  private
+
+  def book_by_choice
+    puts 'Select a book from the following list (enter the corresponding number):'
+    @books.each.with_index(1) do |book, i|
+      puts "#{i}. #{book.title} by #{book.author}"
+    end
+
+    book_choice = gets.chomp.to_i
+    @books[book_choice - 1]
+  end
+
+  def person_by_choice
+    puts 'Select a person from the following list (enter the corresponding number):'
+    @people.each.with_index(1) do |person, i|
+      puts "#{i}. [#{person.class}] name: #{person.name}, ID: #{person.id}, age: #{person.age}"
+    end
+
+    person_choice = gets.chomp.to_i
+    @people[person_choice - 1]
+  end
+end
--- main.rb
+++ main.rb
@@ -1,30 +1,45 @@
 require_relative 'app'
 
-class Main 
-    def initialize()
-        @app = App.new
-    end
+class Main
+  def initialize()
+    @app = App.new
+  end
 
-    def display_menu
-        puts 'Welcome to school library App'
-        puts 'Please choose an option to enter in a number'
-        puts '1. List all books'
-        puts '2. List all people'
-        puts '3. Create a person'
-        puts '4. Create a book'
-        puts '5. Create a rental'
-        puts '6. List rentals for a given person id'
-        puts "7. Quit\n"
-      end
+  def display_menu
+    puts 'Welcome to school library App'
+    puts 'Please choose an option to enter in a number'
+    puts '1. List all books'
+    puts '2. List all people'
+    puts '3. Create a person'
+    puts '4. Create a book'
+    puts '5. Create a rental'
+    puts '6. List rentals for a given person id'
+    puts "7. Quit\n"
+  end
 
-      def run
-        loop do
-          @app.run(display_menu)
-        end
-      end
+  def run
+    # loop do
+    # display_menu
+    choice = gets.chomp.to_i
+
+    # make dictionary for the options
+    choice_options = {
+      1 => @app.list_books,
+      2 => @app.list_people,
+      3 => @app.create_person,
+      4 => @app.create_book,
+      5 => @app.create_rental,
+      6 => @app.list_rentals_for_person,
+      7 => (lambda {
+              puts 'Thank you for using this app!'
+              exit
+            })
+    }
+
+    choice_options[choice]
+  end
 end
 
 
 main = Main.new
 main.run
-
